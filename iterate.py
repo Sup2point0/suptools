bf49f88229ad3af794b9f8d5c8b9d50ae5f6c378
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2023-01-01 15:05:24 +0000

    initialyze

---
+++ iterate.py
@@ -0,0 +1,21 @@
+# functions involving iteration
+
+
+def bubble(iterable, size: int) -> list:
+  '''Iterate through an iterable in a 'bubble' of `size`.
+  
+  ```py
+  >>> l = [1, 2, 3, 4, 5, 6, 7]
+  >>> bubble(l, 3)
+  [[1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, 6], [5, 6, 7]]
+  ```
+  '''
+  
+  result = []
+  
+  i = 0
+  while len(iterable) - size >= i:
+    result.append(iterable[i:i+size])
+    i += 1
+  
+  return result
