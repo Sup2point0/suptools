6bd0ce4dc149ae13ccae26c763df4dd389cd44ea
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2023-01-03 12:24:15 +0000

    realyze

--- foundation.py
+++ foundation.py
@@ -1,4 +1,16 @@
 class object:
+  '''Generic object with any number of attributes.'''
+  
   def __init__(self, **attributes):
     for attribute in attributes:
       self.__setattr__(attribute, attributes[attribute])
+
+
+class series(list):
+  '''1-indexed list.'''
+  
+  def __init__(self, /, *args):
+    super().__init__([*args])
+  
+  def __getitem__(self, index):
+     return super().__getitem__(index - 1)
