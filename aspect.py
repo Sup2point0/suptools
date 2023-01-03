cc6ff985e62ae03057d6ac4cbfa960d17fa8e0ec
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2023-01-03 12:12:47 +0000

    initialyze

---
+++ aspect.py
@@ -0,0 +1,20 @@
+from textwrap import dedent
+
+
+def ground(content: str) -> str:
+  '''Remove common indentation from text, and strip leading and trailing whitespace.
+  
+  ```py
+  >>> s = """
+          sup
+          suppety
+          sup
+  """
+  >>> print(ground(s))
+  sup
+  suppety
+  sup
+  ```
+  '''
+  
+  return dedent(content).strip()
