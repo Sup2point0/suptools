45a90b6e8685e93e13bd1b4511a7bd4de5902b7e
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2023-01-05 12:26:01 +0000

    initialyze

---
+++ time.py
@@ -0,0 +1,19 @@
+# functions related to time
+
+
+def display(s: int):
+  '''Display seconds as written time.
+  '''
+  
+  if s / 86400 >= 1:
+    if s // 360 % 360 >= 0:
+      return f"{int(s // 86400)}d {int(s // 3600 % 24)}h"
+    return f"{int(s // 86400)}d"
+  elif s / 60 >= 60:
+    if s // 60 % 60 > 0:
+      return f"{int(s // 3600)}h {int(s // 60 % 60)} min"
+    return f"{int(s // 3600)}h"
+  elif s >= 60:
+    return f"{int(s // 60)} min"
+  else:
+    return "a few seconds"
