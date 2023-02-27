dd151829d8a8d4853fdde8d3d0a566634be61974
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2023-02-27 15:27:41 +0000

    realyze

--- recurse.py
+++ recurse.py
@@ -19,3 +19,30 @@ def flatten(iterable) -> list:
         yield item
   
   return list(flattened(iterable))
+
+
+def attain(source, *, depth = True) -> list:
+  '''Obtain all objects in the entire directory tree of an object, such as a class or module.
+
+  By default the value of the object is output; if `depth` is `False` the name will be output instead.
+
+  ```py
+  >>> class Test:
+        sup = 2.0
+
+        class Tools:
+          nova = 4.2
+          shard = 6.9
+  
+  >>> [i for i in attain(Test)]
+  [2.0, 4.2, 6.9]
+  >>> [i for i in attain(Test, depth = False)]
+  ['nova', 'shard', 'sup']
+  ```
+  '''
+  
+  for item in [i for i in dir(source) if not i.startswith("__")]:
+    if not isclass(getattr(source, item)):
+      yield getattr(source, item) if depth else item
+    else:
+      yield from attain(getattr(source, item), depth = depth)
