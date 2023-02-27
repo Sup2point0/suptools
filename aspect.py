df3f5c6d0b9c522ad6c4ee55977a89526d658e2d
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2023-02-27 19:40:32 +0000

    realyze

--- aspect.py
+++ aspect.py
@@ -6,6 +6,58 @@ import json
 from textwrap import dedent
 
 
+def stylize(content: str, *, quotes = True, dashes = True) -> str:
+  '''Stylize text with slanted quotes and full dashes.
+  
+  If `quotes` is set to:
+  `True`: Change all to slanted quote marks.
+  `False`: Change all to slanted quote marks.
+  `None`: No change.
+  
+  If `dashes` is set to:
+  `True`: Add en and em dashes.
+  `False`: Remove all en and em dashes.
+  `None`: Do not stylize dashes.
+  '''
+  
+  def style(text: str, ctx: dict):
+    for key in ctx:
+      if ctx[key]:
+        self = text.replace(key, ctx[key])
+    return self
+  
+  if quotes != None:
+    if quotes:
+      content = style(content, {
+        "'": "’",
+        "\"": "”",
+        " ’": " ‘",
+        " ”": " “",
+      }
+    else:
+      content = style(content, {
+        "‘": "'",
+        "’": "'",
+        "“": "\"",
+        "”": "\"",
+      }
+  
+  if dashes != None:
+    if dashes:
+      content = style(content, {
+        " - ": " – ",
+        " -- ": " – ",
+      }
+    else:
+      content = style(content, {
+        "–": "-",
+        " — ": " - ",
+        "—": "-",
+      }
+  
+  return content
+
+
 def assort(content, sep: str = ". ", *, line: str = "\n") -> str:
   '''Arrange items of an iterable in a numbered list.
   
