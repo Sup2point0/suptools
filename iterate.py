bde623a4e2094e548dca9146ceb8761ed0b7750f
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2023-11-20 15:47:44 +0000

    add has and index_any functions

--- iterate.py
+++ iterate.py
@@ -20,6 +20,50 @@ def bubble(iterable, size: int) -> list:
   return result
 
 
+def has(iterable, *values, every = False) -> bool:
+  '''Check if `iterable` contains any of `values`.
+  
+  If `every` is `True`, it must contain every specified value.
+
+  ```py
+  >>> l = ['sup', 'nova', 'shard']
+  >>> has(l, 'soup')
+  False
+  >>> has(l, 'sup')
+  True
+  >>> has(l, 'sup', 'shard')
+  True
+  >>> has(l, 'sup', 'soup')
+  True
+  >>> has(l, 'sup', 'soup', every = True)
+  False
+  ```
+  '''
+
+  out = (each in iterable for each in values)
+  return all(out) if every else any(out)
+
+
+def index_any(iterable, *values, check = False) -> int | tuple[int, any] | None:
+  '''Find index of first occurence of any value within `values`.
+
+  If `check` is True, a tuple containing which value was found is returned instead.
+  
+  Returns `None` if no occurrences are found.
+  '''
+
+  for i, each in enumerate(iterable):
+    if each in values:
+      if check:
+        for value in values:
+          if value == each:
+            return (i, value)
+      else:
+        return i
+  
+  return None
+
+
 def indices(source, content) -> list[int]:
   '''Find indices of all occurences of `content` in an iterable.
   
