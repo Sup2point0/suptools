c78a7a4653a2134581d06b4734be185c08e484e0
Sup#2.0 <sup2point0@gmail.com>
2024-04-09 17:21:23 +0100

    add `io.restructure_json()`

--- source/io.py
+++ source/io.py
@@ -6,3 +6,11 @@ def overwrite(file, content: str):
   file.seek(0)
   file.write(content)
   file.truncate()
+
+
+def restructure_json(file, indent = 2):
+  '''Reformat contents of `file`.'''
+
+  data = json.load(file)
+  content = json.dumps(data, indent = indent)
+  overwrite(file, content)
