da15a5c75d378fd7837f4f885e7ce15ed1054473
Sup#2.0 <sup2point0@gmail.com>
2024-04-09 13:33:27 +0100

    add `tests/search.py`

---
+++ tests/search.py
@@ -0,0 +1,7 @@
+from source import search
+
+def test_search_dict():
+  source = {"sup": 2.0, "super": {"nova": 4.2}}
+
+  assert search.in_dict(source, "sup") == 2.0
+  assert search.in_dict(source, "nova") == 4.2
