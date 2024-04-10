8b583f844b70ef4501a1f296c9ce0e6b7cf99c27
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-04-10 21:07:54 +0100

    update `__init__.py`

--- source/__init__.py
+++ source/__init__.py
@@ -1,2 +1,13 @@
 from . import io
 from . import search
+from . import time
+from .core import *
+
+from .version import __version__
+
+__doc__ = '''
+# `suptools`
+`v{version}`
+
+Uility functions and classes.
+'''
