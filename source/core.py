f4b16f89f276032ec32815d7feffb77ed738675b
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-04-10 21:21:16 +0100

    add `core.init()`

--- source/core.py
+++ source/core.py
@@ -3,6 +3,12 @@ Implements fundamental utility functions and classes.
 '''
 
 
+def init(obj, **attributes):
+  '''Initialise an object with the given attributes.'''
+
+  Object.__init__(obj, **attributes)
+
+
 class Object:
   '''Generic object with any attributes.'''
   
@@ -16,22 +22,3 @@ class Object:
     '''Return a clean string representation of the object’s properties.'''
     
     return "object(" + ", ".join(f"{each} = {vars(self)[each]}" for each in vars(self)) + ")"
-
-
-class Series(list):
-  '''1-indexed list. Supports all of the functionality of a regular list.'''
-  
-  def __init__(self, /, *args):
-    super().__init__([*args])
-  
-  def __getitem__(self, index):
-    if isinstance(index, slice):
-      # support for slice indexing (under construction)
-      return [super(series, self).__getitem__(i - 1) for i in range(
-        index.start or 1,
-        index.stop or len(self) + 1,
-        abs(index.step) or 1,
-      )][::round(index.step / abs(index.step))]
-      # reverses list if step is negative, likely faulty
-    
-    return super(series, self).__getitem__(index - 1)
