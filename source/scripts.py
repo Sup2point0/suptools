1801a2591df598bdbc0768cd6ee6e064c6c57b39
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-04-21 10:54:20 +0100

    update `scripts.run()` to track vitals

--- source/scripts.py
+++ source/scripts.py
@@ -5,11 +5,18 @@ Implements the `Script` class for running scripts with status messages.
 from typing import Callable
 
 from .logging import log
+from .vitality import vitals
 
 
-def run(script: Callable):
+def run(script: Callable, vitals = True):
   '''Run a script with status messages when execution starts and ends.'''
   
   log(status = "Running!")
-  script()
+
+  if vitals:
+    if not hasattr(script, "vitals"):
+      script = vitals(view = True)(script)
+  
+  return script()
+  
   log(status = "Done!")
