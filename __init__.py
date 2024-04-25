201d77c8be3c33cabe02ee8b0764d9c7e1a3f742
Sup#2.0 <sup2point0@gmail.com>
2024-04-25 18:18:01 +0100

    re-add `weighted-list` submodule

--- __init__.py
+++ __init__.py
@@ -6,3 +6,8 @@ Uility functions and classes.
 '''
 
 from .source import *
+
+# importing weighted-list takes a little fiddling,
+# since it has a hyphen in the name...
+import importlib
+WeightedList = importlib.import_module(".weighted-list.Python.weightedlist", "suptools").WeightedList
