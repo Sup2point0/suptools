6bc47ba039c5b9f88348fb8e7191480430192e12
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-05-07 15:49:44 +0100

    debug working directory

--- tests/test_io.py
+++ tests/test_io.py
@@ -2,6 +2,8 @@ import base64
 
 import suptools as sup
 
+import sys; print(sys.path[0])
+
 
 def test_decode_base64_lines():
   c = "sup\nsuppety sup\nsup sup’s sups sup sup’s sup sup sup sup’s sup sups".encode()
