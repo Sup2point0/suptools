5a62cb4540b32de0422ffdca1418f08230a3fd1b
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-05-07 17:04:55 +0100

    remove `weighted-list` import in project `__init__.py`

--- __init__.py
+++ __init__.py
@@ -6,8 +6,3 @@ Uility functions and classes.
 '''
 
 from .source import *
-
-# importing weighted-list takes a little fiddling,
-# since it has a hyphen in the name...
-import importlib
-WeightedList = importlib.import_module(".weighted-list.python", "suptools").WeightedList
