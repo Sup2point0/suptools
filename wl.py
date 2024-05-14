959a68f4405a9981b83a00228e6eca684d112952
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-05-14 12:39:25 +0100

    level up imports

--- wl.py
+++ wl.py
@@ -4,4 +4,9 @@ Interface for importing `weighted-list` via `suptools`.
 
 # importing weighted-list takes a little fiddling, since it has a hyphen in the name...
 import importlib
-WeightedList = importlib.import_module(".weighted-list.python", "suptools").WeightedList
+wl = importlib.import_module(".weighted-list.python", "suptools")
+
+__all__ = ["WeightedItem", "WeightedList"]
+
+WeightedItem = wl.WeightedItem
+WeightedList = wl.WeightedList
